1ee55f6bd188e42534d0abe51937f5d4ae719870
bi_bimbap <seahjm96040207@gmail.com>
2022-11-21 04:57:53 +0000

    Added functionality to trigger makeAnEpicNFT() from smart contract & return OpenSea link to users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { ethers } from "ethers";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import myEpicNft from './utils/MyEpicNFT.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,6 +11,7 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+      const CONTRACT_ADDRESS = "0x97c06A42770d52dF03C83944e3d989fe21fF8E2F";
   // state var to store user's public wallet
   const [currentAccount, setCurrentAccount] = useState("");
   
@@ -35,12 +38,14 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account: ", account);
       setCurrentAccount(account);
+      setupEventListener();
     }
       
   };
 
   const connectWallet = async () => {
     try {
+      // const goerliChainId = "0x5"; // hex code of Goerli network chain ID
       const { ethereum } = window;
 
       if (!ethereum) {
@@ -54,10 +59,80 @@ const App = () => {
       // print public addr once authorize MetaMask
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
+
+      // let chainId = await ethereum.request({ method: 'eth_chainId' });
+      // console.log("Connected to chain " + chainId);
+
+      // if (chainId == goerliChainId) {
+      //   // request access to account
+      //   const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+      //   // print public addr once authorize MetaMask
+      //   console.log("Connected", accounts[0]);
+      //   setCurrentAccount(accounts[0]);
+      //   setupEventListener();
+      // }
+      // else {
+      // 	alert("You are not connected to the Goerli Test Network!");
+      // }
     }
     catch (error) {
       console.log(error);
       
+    }
+  };
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction : https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+      }
+      else {
+        console.log("Ethereum object does not exist!");
+      }
+    } 
+    catch (error) {
+      console.log(error);
+    }
+      
+  };
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // listen for NewEpicNFTMinted event from smart contract
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(`Hey there! We've minted your NFT & sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+        });
+
+        console.log("Setup event listener");
+      }
+      else {
+        console.log("Ethereum object does not exist!");
+      }
+    }
+    catch (error) {
+      console.log(error);
     }
   };
   
@@ -84,7 +159,7 @@ const App = () => {
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
